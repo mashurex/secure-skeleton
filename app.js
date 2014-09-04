679d002ae84c6216ef74e7b949b6f5c6c4c9e21a
Mustafa Ashurex <ashurexm@gmail.com>
2014-09-04 08:38:15 -0700

    Added helmet and reorganized app.use statements

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var compression = require('compression');
 var csrf = require('csurf');
-
+var helmet = require('helmet');
 var routes = require('./routes/index');
 var config = require('./config');
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,11 +19,7 @@ app.set('view engine', 'jade');
 
 app.disable('x-powered-by');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-
 app.use(logger('dev'));
-app.use(csrf());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser(config.app.cookie.secret));
@@ -38,9 +34,12 @@ app.use(session({
         // secure: true // (HTTPS only)
     }
 }));
+
 app.use(compression({
     // threshold: 512 // Sets the min filesize threshold before compression.
 }));
+
+app.use(csrf());
 
 app.use(config.app.path, routes);
 
